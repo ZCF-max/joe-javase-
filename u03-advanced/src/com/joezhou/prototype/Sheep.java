863149424e4f99a4de25621dc2e03f1f6d370464
joezhou <yy06200210@163.com>
2020-08-07 10:53:27 +0800

    深克隆

--- u03-advanced/src/com/joezhou/prototype/Sheep.java
+++ u03-advanced/src/com/joezhou/prototype/Sheep.java
@@ -1,11 +1,12 @@
 package com.joezhou.prototype;
 
+import java.io.*;
 import java.util.Date;
 
 /**
  * @author JoeZhou
  */
-public class Sheep implements Cloneable {
+public class Sheep implements Serializable {
     String name;
     Date birth;
 
@@ -26,6 +27,35 @@ public class Sheep implements Cloneable {
             result.birth = (Date) result.birth.clone();
         } catch (CloneNotSupportedException e) {
             e.printStackTrace();
+        }
+        return result;
+    }
+
+    public Sheep deepClone(Sheep sheep) {
+        Sheep result = null;
+        ObjectOutputStream oos = null;
+        ObjectInputStream ois = null;
+        try {
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            oos = new ObjectOutputStream(baos);
+            oos.writeObject(sheep);
+            oos.flush();
+            ByteArrayInputStream bais = new ByteArrayInputStream(baos.toByteArray());
+            ois = new ObjectInputStream(bais);
+            result = (Sheep) ois.readObject();
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (oos != null) {
+                    oos.close();
+                }
+                if (ois != null) {
+                    ois.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
         return result;
     }
--- u03-advanced/src/com/joezhou/prototype/SheepCloneTest.java
+++ u03-advanced/src/com/joezhou/prototype/SheepCloneTest.java
@@ -12,38 +12,35 @@ public class SheepCloneTest {
 
     @Test
     public void shallowClone() {
-
         Date birth = new Date();
-
         Sheep sheepA = new Sheep();
         sheepA.name = "dorset";
         sheepA.birth = birth;
-        System.out.println("sheepA：" + sheepA.name + "-" + sheepA.birth);
-
         Sheep sheepB = sheepA.shallowClone(sheepA);
-        System.out.println("sheepB：" + sheepB.name + "-" + sheepB.birth);
-        System.out.println(sheepA == sheepB ? "is same sheep" : "is different sheep");
-
+        System.out.println(sheepB);
         birth.setTime(999999999999L);
-        System.out.println("sheepA-birth：" + sheepA.birth);
-        System.out.println("sheepB-birth：" + sheepB.birth);
     }
 
     @Test
     public void multipleShallowClone() {
         Date birth = new Date();
-
         Sheep sheepA = new Sheep();
         sheepA.name = "dorset";
         sheepA.birth = birth;
-        System.out.println("sheepA：" + sheepA.name + "-" + sheepA.birth);
-
         Sheep sheepB = sheepA.multipleShallowClone(sheepA);
-        System.out.println("sheepB：" + sheepB.name + "-" + sheepB.birth);
-        System.out.println(sheepA == sheepB ? "is same sheep" : "is different sheep");
-
         birth.setTime(999999999999L);
-        System.out.println("sheepA-birth：" + sheepA.birth);
-        System.out.println("sheepB-birth：" + sheepB.birth);
+        System.out.println(sheepB);
+    }
+
+    @Test
+    public void deepClone() {
+        Date birth = new Date();
+        Sheep sheepA = new Sheep();
+        sheepA.name = "dorset";
+        sheepA.birth = birth;
+        Sheep sheepB = sheepA.deepClone(sheepA);
+        sheepB.name = "dolly";
+        birth.setTime(999999999999L);
+        System.out.println(sheepB);
     }
 }
