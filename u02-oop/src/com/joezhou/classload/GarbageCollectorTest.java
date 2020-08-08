d0f268593074873d5321c14f286bb99622498004
joezhou <yy06200210@163.com>
2020-08-08 16:02:16 +0800

    u02 内存模型

---
+++ u02-oop/src/com/joezhou/classload/GarbageCollectorTest.java
@@ -0,0 +1,19 @@
+package com.joezhou.classload;
+
+
+import org.junit.Test;
+
+import java.lang.management.GarbageCollectorMXBean;
+import java.lang.management.ManagementFactory;
+
+/**
+ * @author JoeZhou
+ */
+public class GarbageCollectorTest {
+    @Test
+    public void myGarbageCollector() {
+        for (GarbageCollectorMXBean e : ManagementFactory.getGarbageCollectorMXBeans()) {
+            System.out.println(e.getName());
+        }
+    }
+}
