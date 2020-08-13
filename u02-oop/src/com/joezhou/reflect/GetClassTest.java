ae9f097f3de10656faa9a23222e8056f4fe8aa0a
joezhou <yy06200210@163.com>
2020-08-13 14:09:14 +0800

    反射：获取类对象的三种方式

---
+++ u02-oop/src/com/joezhou/reflect/GetClassTest.java
@@ -0,0 +1,31 @@
+package com.joezhou.reflect;
+
+import org.junit.Test;
+
+/**
+ * @author JoeZhou
+ */
+public class GetClassTest {
+    @Test
+    public void getClassByInstance() {
+        GetClassTest instance = new GetClassTest();
+        Class<?> class01 = instance.getClass();
+        Class<?> class02 = instance.getClass();
+        System.out.println(class01.hashCode() == class02.hashCode());
+    }
+
+    @Test
+    public void getClassByClassName() {
+        Class<?> class01 = GetClassTest.class;
+        Class<?> class02 = GetClassTest.class;
+        System.out.println(class01.hashCode() == class02.hashCode());
+    }
+
+    @Test
+    public void getClassByForName() throws ClassNotFoundException {
+        String qualifiedName = "com.joezhou.reflect.GetClassTest";
+        Class<?> class01 = Class.forName(qualifiedName);
+        Class<?> class02 = Class.forName(qualifiedName);
+        System.out.println(class01.hashCode() == class02.hashCode());
+    }
+}
