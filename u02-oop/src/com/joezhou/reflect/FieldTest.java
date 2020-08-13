6379655147f47d087d96d2d06f7a8212deecdf67
joezhou <yy06200210@163.com>
2020-08-13 16:05:33 +0800

    反射：使用成员属性

--- u02-oop/src/com/joezhou/reflect/FieldTest.java
+++ u02-oop/src/com/joezhou/reflect/FieldTest.java
@@ -37,4 +37,37 @@ public class FieldTest {
         System.out.println(klass.getDeclaredField("name"));
         System.out.println(klass.getDeclaredField("gender"));
     }
+
+    @Test
+    public void usePublicField() throws Exception {
+        // FieldTest instanceA = new FieldTest();
+        // FieldTest instanceB = new FieldTest();
+        Object instanceA = klass.getDeclaredConstructor().newInstance();
+        Object instanceB = klass.getDeclaredConstructor().newInstance();
+
+        Field nameField = klass.getField("name");
+
+        // instanceA.name = "赵四"
+        // instanceB.name = "刘能"
+        nameField.set(instanceA, "赵四");
+        nameField.set(instanceB, "刘能");
+
+        // instanceA.name
+        // instanceB.name
+        System.out.println(nameField.get(instanceA));
+        System.out.println(nameField.get(instanceB));
+    }
+
+    @Test
+    public void usePrivateField() throws Exception {
+        Object instanceA = klass.getDeclaredConstructor().newInstance();
+        Object instanceB = klass.getDeclaredConstructor().newInstance();
+        Field ageField = klass.getDeclaredField("gender");
+        ageField.setAccessible(true);
+        ageField.setInt(instanceA, 0);
+        ageField.setInt(instanceB, 1);
+        System.out.println(ageField.get(instanceA));
+        System.out.println(ageField.get(instanceB));
+    }
+
 }
