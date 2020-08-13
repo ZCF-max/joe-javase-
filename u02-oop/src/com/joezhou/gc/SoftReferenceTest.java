aa8482ec64c8e68d3fdd0654e71b54e5d1446ebf
joezhou <yy06200210@163.com>
2020-08-13 09:37:00 +0800

    jvm内存-引用计数测试

--- u02-oop/src/com/joezhou/gc/SoftReferenceTest.java
+++ u02-oop/src/com/joezhou/gc/SoftReferenceTest.java
@@ -11,11 +11,11 @@ public class SoftReferenceTest {
         System.gc();
         TimeUnit.SECONDS.sleep(1L);
         SoftReference<byte[]> softReference = new SoftReference<>(new byte[1024 * 1024 * 10]);
-        System.out.println(softReference.get() == null ? "gc" : "exists");
+        System.out.println(softReference.get() == null ? "be recycled" : "not recycled");
         System.gc();
         TimeUnit.SECONDS.sleep(1L);
-        System.out.println(softReference.get() == null ? "gc" : "exists");
+        System.out.println(softReference.get() == null ? "be recycled" : "not recycled");
         byte[] bsB = new byte[1024 * 1024 * 11];
-        System.out.println(softReference.get() == null ? "gc" : "exists");
+        System.out.println(softReference.get() == null ? "be recycled" : "not recycled");
     }
 }
