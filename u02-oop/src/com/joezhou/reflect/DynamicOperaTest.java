f7bd858fca48f4dd7c93173b2d6b18306742203d
joezhou <yy06200210@163.com>
2020-08-14 14:56:00 +0800

    反射：动态编译

--- u02-oop/src/com/joezhou/reflect/DynamicOperaTest.java
+++ u02-oop/src/com/joezhou/reflect/DynamicOperaTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 
 import javax.tools.JavaCompiler;
 import javax.tools.ToolProvider;
+import java.io.File;
 import java.lang.reflect.Method;
 import java.net.URL;
 import java.net.URLClassLoader;
@@ -14,29 +15,20 @@ import java.net.URLClassLoader;
 public class DynamicOperaTest {
 
     @Test
-    public void dynamicCompile() throws Exception {
-        // 获取系统编辑器
-        JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
-        // p1：inputStream输入流，传递给javac的数据，如果为null，表示使用System.in
-        // p2：outputStream输出流，javac返回的数据，如果为null，表示使用System.out
-        // p3：outputStream输出流，javac返回的错误信息，如果为null，表示使用System.err
-        // p4：String类型的不定长数组，可以传递一个或者多个java源文件
-        int result = compiler.run(null, null, null, "E:\\HelloWorld.java");
-        System.out.println(result);// 0代表编译成功，1代表编译失败
+    public void dynamicCompile() {
+        JavaCompiler javac = ToolProvider.getSystemJavaCompiler();
+        String filePath = "D:" + File.separator + "HelloWorld.java";
+        int result = javac.run(null, null, null, filePath);
+        System.out.println(result == 0 ? "javac success" : "javac success");
     }
 
     @Test
     public void dynamicRun() throws Exception {
-        URL url = new URL("file:/E:/");
-        // 类加载器：把加载到内存中
+        URL url = new URL("file:" + File.separator + "D:" + File.separator);
         URLClassLoader urlClassLoader = new URLClassLoader(new URL[]{url});
-        // 加载类名
-        Class<?> loadClass = urlClassLoader.loadClass("HelloWorld");
-        // 获取方法：main方法
-        // p1：调用的方法的名字
-        // p2：当你调用这个方法的时候传进来的参数类型
-        Method method = loadClass.getMethod("main", String[].class);
-        // invoke 是真正的在调用这个方法
+        Class<?> klass = urlClassLoader.loadClass("HelloWorld");
+        Method method = klass.getMethod("main", String[].class);
+        // main() is static, so p1 of invoke() must be null
         // 这里必须强转成Object类型，否则String[]数组参数会被拆成"a"和"b",与main方法的参数个数不符合
         method.invoke(null, (Object) new String[]{"a", "b"});
         urlClassLoader.close();
