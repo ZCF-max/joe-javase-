50d5a4e651abb4df96588236dc4dfda85a5b3b21
joezhou <yy06200210@163.com>
2020-08-17 14:04:33 +0800

    泛型

--- u03-advanced/src/com/joezhou/generic/PointTest.java
+++ u03-advanced/src/com/joezhou/generic/PointTest.java
@@ -10,6 +10,10 @@ public class PointTest {
     private static class Point {
         private Integer integerX;
         private Integer integerY;
+        private Double doubleX;
+        private Double doubleY;
+        private String stringX;
+        private String stringY;
 
         Integer getIntegerX() {
             return integerX;
@@ -26,6 +30,38 @@ public class PointTest {
         void setIntegerY(Integer integerY) {
             this.integerY = integerY;
         }
+
+        Double getDoubleX() {
+            return doubleX;
+        }
+
+        void setDoubleX(Double doubleX) {
+            this.doubleX = doubleX;
+        }
+
+        Double getDoubleY() {
+            return doubleY;
+        }
+
+        void setDoubleY(Double doubleY) {
+            this.doubleY = doubleY;
+        }
+
+        String getStringX() {
+            return stringX;
+        }
+
+        void setStringX(String stringX) {
+            this.stringX = stringX;
+        }
+
+        String getStringY() {
+            return stringY;
+        }
+
+        void setStringY(String stringY) {
+            this.stringY = stringY;
+        }
     }
 
     @Test
@@ -34,11 +70,11 @@ public class PointTest {
         point.setIntegerX(10);
         point.setIntegerY(20);
         System.out.println(point.getIntegerX() + " : " + point.getIntegerY());
-        //point.setDoubleX(10.5);
-        //point.setDoubleY(20.5);
-        //System.out.println(point.getDoubleX() + " : " + point.getDoubleY());
-        //point.setStringX("东经108°25′");
-        //point.setStringY("东经108°25′");
-        //System.out.println(point.getStringX() + " : " + point.getStringY());
+        point.setDoubleX(10.5);
+        point.setDoubleY(20.5);
+        System.out.println(point.getDoubleX() + " : " + point.getDoubleY());
+        point.setStringX("东经108°25′");
+        point.setStringY("北纬108°25′");
+        System.out.println(point.getStringX() + " : " + point.getStringY());
     }
 }
