9ee4ef32b7dde0b6ad01dd508b16f04e90b10bb3
joezhou <yy06200210@163.com>
2020-08-21 14:22:17 +0800

    ArrayList-遍历

--- u03-advanced/src/com/joezhou/collection/ArrayListTest.java
+++ u03-advanced/src/com/joezhou/collection/ArrayListTest.java
@@ -25,19 +25,19 @@ public class ArrayListTest {
         list.add("China");
         list.add("Japan");
         list.add(1, "Korea");
-        list.add(1,"Thailand");
+        list.add(1, "Thailand");
 
         List<String> europe = new ArrayList<>();
         europe.add("UK");
         europe.add("France");
         list.addAll(europe);
 
-        List<String> africa  = new ArrayList<>();
+        List<String> africa = new ArrayList<>();
         africa.add("Congo");
         africa.add("Egypt");
         list.addAll(1, africa);
 
-        System.out.println(list);
+        System.out.println("add over: " + list);
     }
 
     @Test
@@ -72,7 +72,7 @@ public class ArrayListTest {
         List<String> europe = new ArrayList<>();
         europe.add("UK");
         europe.add("France");
-        List<String> africa  = new ArrayList<>();
+        List<String> africa = new ArrayList<>();
         africa.add("Congo");
         africa.add("Egypt");
 
@@ -85,41 +85,46 @@ public class ArrayListTest {
     }
 
     @Test
-    public void iterator() {
-        List<String> list = new ArrayList<String>();
-        list.add("中国");
-        list.add("印度");
-        list.add("美国");
-
-        System.out.println("\n\n使用for遍历list...");
+    public void iteratorByFor() {
+        this.add();
         for (int i = 0, j = list.size(); i < j; i++) {
-            System.out.print(list.get(i) + "\t");
-        }
-
-        System.out.println("\n\n使用foreach遍历list...");
-        for (String e : list) {
-            System.out.print(e + "\t");
-        }
-
-        // ListIterator是List接口提供的特殊的迭代器，是Iterator的子类，只能遍历list，用法和iterator一致
-        // ListIterator除了Iterator接口提供的正常操作外，还额外提供了hasPrevious()、previous()、set()等方法
-        System.out.println("\n\n使用listIterator遍历list...");
-        ListIterator<String> lit1 = list.listIterator();// 获取list迭代器对象，指针置于首位
-        while (lit1.hasNext()) {// 如果有下一个元素
-            String e = lit1.next();// 获取下一个元素
-            if ("中国".equals(e)) {// 如果当前元素是"中国"，则改为"中华人民共和国"
-                lit1.set("中华人民共和国");
-            }
-            if ("印度".equals(e)) {// 如果当前元素是"印度'，则删除这个元素
-                lit1.remove();
-            }
-            System.out.print(e + "\t");// 此时输出的结果仍是["中国 印度 美国"]，但原list中的元素已经改为["中华人民共和国 美国"]
-        }
-
-        System.out.println("\n\n使用listIterator反向遍历list...");
-        ListIterator<String> lit2 = list.listIterator(2);// 获取list迭代器对象，指针置于2号位置
-        while (lit2.hasPrevious()) {// 如果有上一个元素
-            System.out.print(lit2.previous() + "\t");// 获取上一个元素并打印
+            System.out.print(list.get(i) + "\0");
         }
     }
+
+    @Test
+    public void iteratorByForEach() {
+        this.add();
+        for (String str : list) {
+            System.out.print(str + "\0");
+        }
+    }
+
+    @Test
+    public void iterator() {
+        this.add();
+        ListIterator<String> listIter = list.listIterator(0);
+        while (listIter.hasNext()) {
+            String e = listIter.next();
+
+            if ("China".equals(e)) {
+                listIter.set("CHINA");
+            }
+            if ("UK".equals(e)) {
+                listIter.remove();
+            }
+            // 这里打印的是set()和remove()的原值，但是list中的内容已经发生了改变
+            System.out.print(e + "\0");
+        }
+    }
+
+    @Test
+    public void reverseIterator() {
+        this.add();
+        ListIterator<String> listIter = list.listIterator(list.size());
+        while (listIter.hasPrevious()) {
+            System.out.print(listIter.previous() + "\0");
+        }
+    }
+
 }
