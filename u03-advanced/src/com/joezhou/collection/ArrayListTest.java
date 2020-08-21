0f0a3cfca49b04ce449d8028ee7dc7e568130782
joezhou <yy06200210@163.com>
2020-08-21 11:09:51 +0800

    ArrayList-api

--- u03-advanced/src/com/joezhou/collection/ArrayListTest.java
+++ u03-advanced/src/com/joezhou/collection/ArrayListTest.java
@@ -22,36 +22,36 @@ public class ArrayListTest {
     @Test
     public void add() {
         list.add("China");
-        list.add("US");
-        list.add(1, "India");
-        list.add("Africa");
-        List<String> newList = new ArrayList<>();
-        newList.add("Canada");
-        newList.add("UK");
-        list.addAll(newList);
-        newList = new ArrayList<>();
-        newList.add("Japan");
-        newList.add("Korea");
-        list.addAll(1, newList);
+        list.add("Japan");
+        list.add(1, "Korea");
+        list.add(1,"Thailand");
+
+        List<String> europe = new ArrayList<>();
+        europe.add("UK");
+        europe.add("France");
+        list.addAll(europe);
+
+        List<String> africa  = new ArrayList<>();
+        africa.add("Congo");
+        africa.add("Egypt");
+        list.addAll(1, africa);
+
         System.out.println(list);
     }
 
     @Test
     public void retrieve() {
-        list.add("China");
-        list.add("US");
-        list.add("Canada");
-        list.add("UK");
-        list.add("Japan");
-        list.add("China");
-        List<String> newList = new ArrayList<>();
-        newList.add("Korea");
-        System.out.println(list.get(5));
+        this.add();
+        List<String> europe = new ArrayList<>();
+        europe.add("UK");
+        europe.add("France");
+
+        System.out.println(list.get(0));
         System.out.println(list.indexOf("China"));
-        System.out.println(list.lastIndexOf("China"));
-        System.out.println(list.equals(newList));
+        System.out.println(list.lastIndexOf("UK"));
+        System.out.println(list.equals(europe));
         System.out.println(list.contains("Japan"));
-        System.out.println(list.containsAll(newList));
+        System.out.println(list.containsAll(europe));
         System.out.println(list.isEmpty());
         System.out.println(list.size());
         System.out.println(list.toArray()[0]);
@@ -59,28 +59,27 @@ public class ArrayListTest {
 
     @Test
     public void update() {
-        list.add("US");
-        list.set(0, "UK");
+        this.add();
+
+        list.set(1, "UK");
         System.out.println(list);
     }
 
     @Test
     public void delete() {
-        list.add("China");
-        list.add("US");
-        list.add("Canada");
-        list.add("UK");
-        list.add("Japan");
-        list.add("China");
+        this.add();
+        List<String> europe = new ArrayList<>();
+        europe.add("UK");
+        europe.add("France");
+        List<String> africa  = new ArrayList<>();
+        africa.add("Congo");
+        africa.add("Egypt");
+
         System.out.println(list.subList(0, 3));
         System.out.println(list.remove(1));
-        System.out.println(list.remove("UK"));
-        ArrayList<String> newList = new ArrayList<>();
-        newList.add("Canada");
-        list.removeAll(newList);
-        newList = new ArrayList<>();
-        newList.add("China");
-        list.retainAll(newList);
+        System.out.println(list.remove("Egypt"));
+        list.removeAll(europe);
+        list.retainAll(africa);
         list.clear();
     }
 }
