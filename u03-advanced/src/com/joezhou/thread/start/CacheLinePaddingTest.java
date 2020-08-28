a8fbbe0605957ac43aad9eeca4f2cc5b0cef65f9
joezhou <yy06200210@163.com>
2020-08-28 14:48:36 +0800

    volatile 有序性

--- u03-advanced/src/com/joezhou/thread/start/CacheLinePaddingTest.java
+++ u03-advanced/src/com/joezhou/thread/start/CacheLinePaddingTest.java
@@ -1,42 +1,53 @@
 package com.joezhou.thread.start;
 
+import lombok.SneakyThrows;
+import org.junit.After;
+import org.junit.Test;
+
 /**
  * @author JoeZhou
  */
 public class CacheLinePaddingTest {
-    public static long COUNT = 1_0000_0000L;
+
+    private final static long COUNT = 1_0000_0000L;
 
     private static class Demo {
-        //public long p1, p2, p3, p4, p5, p6, p7;
-        /**
-         * 一个缓存行只有64字节，所以这个x不会同时出现在两个缓存航中
-         */
+        public long p1, p2, p3, p4, p5, p6, p7;
         public volatile long x = 0L;
-        //public long p9, p10, p11, p12, p13, p14, p15;
+        public long p9, p10, p11, p12, p13, p14, p15;
     }
 
-    public static Demo[] demos = new Demo[2];
+    private static Demo[] demos = new Demo[2];
 
-    static{
+    static {
         demos[0] = new Demo();
         demos[1] = new Demo();
     }
 
-    public static void main(String[] args) {
-        new Thread(()->{
+    @Test
+    public void cacheLinePaddingEfficiency() {
+        new Thread(() -> {
             long start = System.currentTimeMillis();
             for (long i = 0; i < COUNT; i++) {
                 demos[0].x = i;
             }
-            System.out.println(System.currentTimeMillis()- start);
+            long end = System.currentTimeMillis();
+            System.out.println(end - start);
         }).start();
 
-        new Thread(()->{
+        new Thread(() -> {
             long start = System.currentTimeMillis();
             for (long i = 0; i < COUNT; i++) {
                 demos[1].x = i;
             }
-            System.out.println(System.currentTimeMillis()- start);
+            long end = System.currentTimeMillis();
+            System.out.println(end - start);
         }).start();
+    }
+
+    @SneakyThrows
+    @After
+    public void after() {
+        System.out.println(System.in.read());
     }
 }
