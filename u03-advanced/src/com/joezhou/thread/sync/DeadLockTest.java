3de3711250d09395b8cf1493050f0e6d1fd969f7
joezhou <yy06200210@163.com>
2020-08-31 11:45:34 +0800

    synchronized 死锁

--- u03-advanced/src/com/joezhou/thread/sync/DeadLockTest.java
+++ u03-advanced/src/com/joezhou/thread/sync/DeadLockTest.java
@@ -1,36 +1,57 @@
 package com.joezhou.thread.sync;
 
-/**@author Joe*/
+import lombok.SneakyThrows;
+import org.junit.After;
+import org.junit.Test;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author Joe
+ */
 public class DeadLockTest {
-	public static void main(String[] args) {
-		DeadLockRunnable r = new DeadLockRunnable();
-		new Thread(r).start();
-		new Thread(r).start();
-	}
+
+    private static class DeadLockRunnable implements Runnable {
+
+        private final Object objA = new Object();
+        private final Object objB = new Object();
+
+        @SneakyThrows
+        @Override
+        public void run() {
+            String threadA = "threadA";
+            if (Thread.currentThread().getName().equals(threadA)) {
+                synchronized (objA) {
+                    System.out.println("if: objA");
+                    TimeUnit.SECONDS.sleep(1L);
+                    synchronized (objB) {
+                        System.out.println("if: objB");
+                    }
+                }
+            } else {
+                synchronized (objB) {
+                    System.out.println("else: objB");
+                    TimeUnit.SECONDS.sleep(1L);
+                    synchronized (objA) {
+                        System.out.println("else: objA");
+                    }
+                }
+            }
+        }
+    }
+
+    @Test
+    public void staticMethodLockType() {
+        Runnable runnable = new DeadLockRunnable();
+        new Thread(runnable, "threadA").start();
+        new Thread(runnable, "threadB").start();
+    }
+
+    @SneakyThrows
+    @After
+    public void after() {
+        System.out.println(System.in.read());
+    }
+
 }
 
-class DeadLockRunnable implements Runnable {
-
-	private final Object obj1 = new Object();
-    private final Object obj2 = new Object();
-
-	@Override
-	public void run() {
-        String t1 = "Thread-0";
-		if (Thread.currentThread().getName().equals(t1)) {
-			synchronized (obj1) {
-				System.out.println("if -- obj1");
-				synchronized (obj2) {
-					System.out.println("if -- obj2");
-				}
-			}
-		} else {
-			synchronized (obj2) {
-				System.out.println("else -- obj2");
-				synchronized (obj1) {
-					System.out.println("else -- obj1");
-				}
-			}
-		}
-	}
-}
