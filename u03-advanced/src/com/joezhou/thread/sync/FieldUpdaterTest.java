14dc2b9afd3ff29902288164d3d39e417e5758a7
joezhou <yy06200210@163.com>
2020-09-01 10:38:42 +0800

    fieldUpdater

--- u03-advanced/src/com/joezhou/thread/sync/FieldUpdaterTest.java
+++ u03-advanced/src/com/joezhou/thread/sync/FieldUpdaterTest.java
@@ -34,8 +34,9 @@ public class FieldUpdaterTest {
 
     @Test
     public void atomicLongFieldUpdater() {
-        AtomicLongFieldUpdater<Student> idUpdater;
-        idUpdater = AtomicLongFieldUpdater.newUpdater(Student.class, "id");
+        AtomicLongFieldUpdater<Student> idUpdater =
+                AtomicLongFieldUpdater.newUpdater(
+                        Student.class, "id");
 
         System.out.println("++id：" + idUpdater.incrementAndGet(student));
         System.out.println("id：" + student.getId());
@@ -43,21 +44,24 @@ public class FieldUpdaterTest {
         System.out.println("--id：" + idUpdater.decrementAndGet(student));
         System.out.println("id：" + student.getId());
 
-        System.out.println("id+=5 then return：" + idUpdater.addAndGet(student, 5));
+        System.out.println("id+=5 then return："
+                + idUpdater.addAndGet(student, 5));
         System.out.println("id：" + student.getId());
 
-        System.out.println("accumulate then return：" + idUpdater.accumulateAndGet(student, 5, (left, right) -> {
-            System.out.print("left：" + left + "\t");
-            System.out.print("right：" + right + "\n");
-            return (left - 1) * (right - 9) / 4;
-        }));
+        System.out.println("accumulate then return：" +
+                idUpdater.accumulateAndGet(student, 5, (left, right) -> {
+                    System.out.print("left：" + left + "\t");
+                    System.out.print("right：" + right + "\n");
+                    return (left - 1) * (right - 9) / 4;
+                }));
         System.out.println("id：" + student.getId());
     }
 
     @Test
     public void atomicIntegerFieldUpdater() {
-        AtomicIntegerFieldUpdater<Student> ageUpdater;
-        ageUpdater = AtomicIntegerFieldUpdater.newUpdater(Student.class, "age");
+        AtomicIntegerFieldUpdater<Student> ageUpdater =
+                AtomicIntegerFieldUpdater.newUpdater(
+                        Student.class, "age");
 
         System.out.println("age++: " + ageUpdater.getAndIncrement(student));
         System.out.println("age：" + student.getAge());
@@ -65,26 +69,31 @@ public class FieldUpdaterTest {
         System.out.println("age--：" + ageUpdater.getAndDecrement(student));
         System.out.println("age：" + student.getAge());
 
-        System.out.println("return then age+=5：" + ageUpdater.getAndAdd(student, 5));
+        System.out.println("return then age+=5："
+                + ageUpdater.getAndAdd(student, 5));
         System.out.println("age：" + student.getAge());
 
-        System.out.println("return then accumulate" + ageUpdater.getAndAccumulate(student, 5, (left, right) -> {
-            System.out.print("left：" + left + "\t");
-            System.out.print("right：" + right + "\n");
-            return (left - 1) * (right - 9) / 4;
-        }));
+        System.out.println("return then accumulate" +
+                ageUpdater.getAndAccumulate(student, 5, (left, right) -> {
+                    System.out.print("left：" + left + "\t");
+                    System.out.print("right：" + right + "\n");
+                    return (left - 1) * (right - 9) / 4;
+                }));
         System.out.println("age：" + student.getAge());
     }
 
     @Test
     public void atomicReferenceFieldUpdater() {
-        AtomicReferenceFieldUpdater<Student, String> nameUpdater;
-        nameUpdater = AtomicReferenceFieldUpdater.newUpdater(Student.class, String.class, "name");
+        AtomicReferenceFieldUpdater<Student, String> nameUpdater =
+                AtomicReferenceFieldUpdater.newUpdater(
+                        Student.class, String.class, "name");
 
-        System.out.println("set name to fei-ji: " + nameUpdater.getAndSet(student, "fei-ji"));
+        System.out.println("set name to fei-ji: " +
+                nameUpdater.getAndSet(student, "fei-ji"));
         System.out.println("name：" + student.getName());
 
-        System.out.println("cas name to fei-ji: " + nameUpdater.compareAndSet(student, "fei-ji", "da-pao"));
+        System.out.println("cas name to fei-ji: " +
+                nameUpdater.compareAndSet(student, "fei-ji", "da-pao"));
         System.out.println("name：" + student.getName());
     }
 }
