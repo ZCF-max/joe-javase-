6fe6db9ee7434439ef5c79b57fa1ba77c20a83b5
joezhou <yy06200210@163.com>
2020-09-07 11:17:21 +0800

    ConcurrentHashMap

--- u03-advanced/src/com/joezhou/thread/collection/SynchronizedMapTest.java
+++ u03-advanced/src/com/joezhou/thread/collection/SynchronizedMapTest.java
@@ -2,10 +2,7 @@ package com.joezhou.thread.collection;
 
 import org.junit.Test;
 
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Hashtable;
-import java.util.Map;
+import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentSkipListMap;
 
@@ -17,29 +14,40 @@ public class SynchronizedMapTest {
     @Test
     public void hashtable() {
         Hashtable<String, Object> hashtable = new Hashtable<>();
-        hashtable.put("name", "赵四");
+        hashtable.put("name", "zhao-si");
         hashtable.put("gender", "male");
         hashtable.put("age", 18);
-        hashtable.forEach((i, v) -> {
-            System.out.println(v);
+        hashtable.forEach((key, value) -> {
+            System.out.println(value);
         });
         System.out.println(hashtable.size());
     }
 
     @Test
     public void synchronizedHashMap() {
-        Map<Object, Object> map = Collections.synchronizedMap(new HashMap<>(3));
+        HashMap<String, Object> hashMap = new HashMap<>(3);
+        Map<String, Object> map = Collections.synchronizedMap(hashMap);
+        map.put("name", "zhao-si");
+        map.forEach((k, v) -> {
+            System.out.println(v);
+        });
     }
 
     @Test
     public void concurrentHashMap() {
-        ConcurrentHashMap<Object, Object> hashMap = new ConcurrentHashMap<>(3);
+        Map<Object, Object> map = new ConcurrentHashMap<>(3);
+        map.put("name", "zhao-si");
+        map.forEach((k, v) -> {
+            System.out.println(v);
+        });
     }
 
     @Test
     public void concurrentSkipListMap() {
-        // ConcurrentSkipListMap是一个跳表数据结构的高并发Map，查询效率高。
-        // 它适用于高并发，且它可以排序，用于替代concurrentTreeMap(不存在，因为树结构的CAS太复杂)
-        Map<Object, Object> hashMap = new ConcurrentSkipListMap<>();
+        Map<Object, Object> map = new ConcurrentSkipListMap<>();
+        map.put("name", "zhao-si");
+        map.forEach((k, v) -> {
+            System.out.println(v);
+        });
     }
 }
